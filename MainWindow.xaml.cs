1056e959981e25cbfe79351a31ec8a840a163c60
JakubCisowski <jakub.cisowski1@gmail.com>
2020-09-04 20:00:38 +0200

    Added basic user interface.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+
+//todo Graphics - maybe change the border colour around focused textbox
+//todo Functionality - create conversion system
 
 namespace NumeralSystemConverter
 {
@@ -23,6 +13,7 @@ namespace NumeralSystemConverter
         public MainWindow()
         {
             InitializeComponent();
+            this.ResizeMode = ResizeMode.NoResize;
         }
     }
 }
